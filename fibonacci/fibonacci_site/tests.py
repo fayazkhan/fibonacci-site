2f4bd83916e620be6d1437f0deae7055e9e81a37
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2016-08-15 08:00:09 +0530

    test index page

--- fibonacci/fibonacci_site/tests.py
+++ fibonacci/fibonacci_site/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
 # Create your tests here.
+
+
+# Acceptance tests.
+class FibonacciTestCase(TestCase):
+
+    def test_index(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
